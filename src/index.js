ef40a88cc983638e46b1830a18cfad46cac92336
Boughous-startup <akinolavictor50@gmail.com>
2026-05-12 22:45:58 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// npm i --save-dev react react-dom
+// npm install rollup rollup-plugin-babel @rollup/plug-node-resolve rollup-plugin-peer-deps-external --save-dev
+// npm i react react-dom rollup rollup-plugin-babel @rollup/plugin-node-resolve rollup-plugin-peer-deps-external --save-dev
+// npm i babel-loader @babel/core @babel/preset-react --save-dev
+
+export * from "./components/texts"
